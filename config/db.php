95ae82a34cf8e6e282757ee5b06e2c2d87c9aae9
stavyan <617946852@qq.com>
2019-01-02 23:35:38 +0800

    初始化Yii项目

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=stavv',
+    'username' => 'root',
+    'password' => 'qq617946852',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
